470f4f9ad0ad56e5e637d88b1084741a5f7ecb32
SeanHolden <sean.holden@o2.com>
2013-04-26 18:12:20 +0100

    Edited the way the modules are defined

--- lib/ivona_speech_cloud/authorization.rb
+++ lib/ivona_speech_cloud/authorization.rb
@@ -1,19 +1,17 @@
 require 'httparty'
 
-module IvonaSpeechCloud
-  module Auth
-    # Make a request for a token
-    def self.get_token
-      email = IvonaSpeechCloud::Config.email
-      options = { :body => "email=#{email}" }
-      HTTParty.post("#{BASE_URL}/tokens/", options)
-    end
+module IvonaSpeechCloud::Auth
+  # Make a request for a token
+  def self.get_token
+    email = IvonaSpeechCloud::Config.email
+    options = { :body => "email=#{email}" }
+    HTTParty.post("#{BASE_URL}/tokens/", options)
+  end
 
-    # Verify that token and md5 are legit. A response of 1 confirms that they are.
-    def self.check_token
-      token  = get_token
-      params = "token=#{token}&md5=#{GetMd5.formula(token)}"
-      HTTParty.get("#{BASE_URL}/tokens?#{params}")
-    end
+  # Verify that token and md5 are legit. A response of 1 confirms that they are.
+  def self.check_token
+    token  = get_token
+    params = "token=#{token}&md5=#{GetMd5.formula(token)}"
+    HTTParty.get("#{BASE_URL}/tokens?#{params}")
   end
 end
--- lib/ivona_speech_cloud/get_md5.rb
+++ lib/ivona_speech_cloud/get_md5.rb
@@ -1,17 +1,15 @@
 require 'digest/md5'
 
-module IvonaSpeechCloud
-  module GetMd5
-    # Formula for md5 param is: md5( md5( api_key ) + token )
-    def self.formula(token)
-      api_key = IvonaSpeechCloud::Config.api_key
-      puts api_key.kind_of? String
-      api_key_as_md5 = to_md5( api_key )
-      to_md5( api_key_as_md5 + token )
-    end
+module IvonaSpeechCloud::GetMd5
+  # Formula for md5 param is: md5( md5( api_key ) + token )
+  def self.formula(token)
+    api_key = IvonaSpeechCloud::Config.api_key
+    puts api_key.kind_of? String
+    api_key_as_md5 = to_md5( api_key )
+    to_md5( api_key_as_md5 + token )
+  end
 
-    def self.to_md5(a_string)
-      Digest::MD5.hexdigest(a_string)
-    end
+  def self.to_md5(a_string)
+    Digest::MD5.hexdigest(a_string)
   end
 end
